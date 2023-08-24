55f513f55045da198846296601e1a13e5a36927c
Raj <rajmkumar369@gmail.com>
2023-08-24 14:14:55 +0530

    pramata weather api backend

---
+++ Pramata_weatherapp_backend-2/src/main/java/com/example/demo/PramataWeatherappBackend2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PramataWeatherappBackend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PramataWeatherappBackend2Application.class, args);
+	}
+
+}
